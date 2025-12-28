3fe5de01bd8e30dc2797c64d31b50fa93ff956b1
karlo-alano <alano.juankarlo@gmail.com>
2025-12-28 14:42:21 +0800

    feat(ui): added proper navbars to mobile view

--- frontend/src/components/NavbarMobile.vue
+++ frontend/src/components/NavbarMobile.vue
@@ -2,24 +2,60 @@
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
+import { useUserStore } from '@/store/userStore';
+const userStore = useUserStore();
+
+import { useFormStore } from '@/store/formStore';
+const formStore = useFormStore();
+
 const createAndRefresh = () => {
-    router.push('/Create');
-    setTimeout(() => {
-        window.location.reload();
-    }, 50);
+    formStore.resetState()
+    router.push('/Create'); 
 };
+
+const registration = () => {
+  const isLoggedIn = userStore.hasProfile;
+  if (!isLoggedIn) {
+    router.push('/Registration')
+  } else {
+    router.push('/Account')
+  }
+}
+
+const isActive = (navPath) => {
+  const path = router.currentRoute.value.path;
+  return path === navPath;
+};
+
 
 const isLoggedIn = false;
 </script>
 
 <template>
     <nav class="h-2rem w-screen z-10">
-        <div class="flex justify-around p-2 gap-3 bg-white shadow-2xl">
-            <Button class="w-15 h-15 bg-white border-0" severity="secondary" icon="pi pi-plus" @click="createAndRefresh" />
-            <Button class="w-15 h-15 bg-white border-0" severity="secondary" icon="pi pi-compass" @click="router.push('/Explore')"/>
-            <Button class="w-15 h-15 bg-white border-0" severity="secondary" icon="pi pi-book" @click="router.push('/Blog')"/>
-            <Button class="w-15 h-15 bg-white border-0" severity="secondary" icon="pi pi-user" />
+        <div class="flex justify-between p-2 gap-1 bg-white shadow-2xl">
+            <div class="nav-item" :class="{ 'nav-item-active': isActive('/Create') }">
+                <Button class="w-20 h-15 bg-white border-0 text-slate-500" severity="secondary" icon="pi pi-plus" @click="createAndRefresh" label="Create" icon-pos="top"/>
+            </div>
+            <div class="nav-item" :class="{ 'nav-item-active': isActive('/Explore') }">
+                <Button class="w-20 h-15 bg-white border-0 text-slate-500" severity="secondary" icon="pi pi-compass" @click="router.push('/Explore')" label="Explore" icon-pos="top"/>
+            </div>
+            <div class="nav-item" :class="{ 'nav-item-active': isActive('/Blog') }">
+                <Button class="w-20 h-15 bg-white border-0 text-slate-500" severity="secondary" icon="pi pi-book" @click="router.push('/Blog')" label="Blogs" icon-pos="top"/>
+            </div>
+            <div class="nav-item" :class="{ 'nav-item-active': isActive('/Registration') || isActive('/Account')}">
+                <Button class="w-20 h-15 bg-white border-0 text-slate-500" severity="secondary" icon="pi pi-user" @click="registration" label="User" icon-pos="top"/>
+            </div>
         </div>
     </nav>
 </template>
+
+<style scoped>
+    @import '@/assets/styles.css';
+
+    .nav-item-active Button{
+    @apply bg-slate-200 font-extrabold text-slate-700
+  }
+
+</style>
 
--- frontend/src/components/NavbarMobileOverlay.vue
+++ frontend/src/components/NavbarMobileOverlay.vue
@@ -9,12 +9,8 @@ const isLoggedIn = false;
     <nav id="topNav" class="bg-transparent p-2 z-10 fixed top-0 w-screen">
         <div class="h-3rem w-screen flex justify-between items-center hover:opacity-80 pointer-cursor">
             <router-link to="/" class="h-12 w-12 flex items-center px-4 bg-transparent">
-                <img src="@/assets/imgs/Itinero_standalone.svg" alt="Logo" class="w-12 h-12 scale-200"/>
+                <img src="@/assets/imgs/ItineroLogo.svg" alt="Logo" class="w-12 h-12 scale-200"/>
             </router-link>
-            <div>
-                <Avatar v-if="isLoggedIn" label="K" class="mr-3 shadow-xl" size="large" style="background-color: #9954DD; color: white" shape="circle" severity="secondary" />
-                <Button label="Sign In" rounded raised class="mr-4 w-25 pointer-events-auto" @click="router.push('/Registration')"></Button>
-            </div>
         </div>
     </nav>
 </template>
